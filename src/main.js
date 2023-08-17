acff42606e613fe0109811e034b2950b2234f4b9
Domas Naujokaitis <domas@digiklase.lt>
2023-08-17 18:30:11 +0300

    Test analytics

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from "./i18n";
+import VueGtag from "vue-gtag";
 
 const app = createApp(App);
 
 app.use(i18n)
 
-app.mount('#app')
+app.use(VueGtag, {
+    config: { id: "G-L98Z3SJY5M" }
+}).mount('#app')
