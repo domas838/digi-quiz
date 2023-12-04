8e4181a4113b1e3b82e192163414752295d42a22
Domas Naujokaitis <domas@digiklase.lt>
2023-12-04 17:44:46 +0200

    Fix state question

--- src/store.js
+++ src/store.js
@@ -538,11 +538,11 @@ export const store = reactive({
                 {
                     id: 'state',
                     qNo: 2,
-                    Q: 'State',
+                    Q: 'In which state do you live?',
                     Ans: [
                         'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
                     ],
-                    placeholder: 'In which state do you live?',
+                    placeholder: 'State',
                     component: 'select'
                 },
                 {
